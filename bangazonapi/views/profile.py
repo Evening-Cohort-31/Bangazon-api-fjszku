5863a4133350d850dcff33f9d70d52f6168c003f
Jason Norman <json.norman@gmail.com>
2026-05-13 20:09:21 -0500

    updated profile.py

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -85,7 +85,7 @@ class Profile(ViewSet):
         try:
             current_user = Customer.objects.get(user=request.auth.user)
             current_user.recommends = Recommendation.objects.filter(
-                recommender=current_user
+                customer=current_user
             )
 
             serializer = ProfileSerializer(
